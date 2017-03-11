2c4f25040e40ea2191f277d5d111d87d3ec14416
luktom <luktom@luktom.net>
2017-03-11 20:27:15 +0100

    Add new extensions

---
+++ Tomaszkiewicz.BotFramework/Extensions/ActivityExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Bot.Connector;
+
+namespace Tomaszkiewicz.BotFramework.Extensions
+{
+    public static class ActivityExtensions
+    {
+        public static ConnectorClient CreateConnectorClient(this IActivity activity)
+        {
+            return new ConnectorClient((new Uri(activity.ServiceUrl)));
+        }
+    }
+}
---
+++ Tomaszkiewicz.BotFramework/Extensions/ConversationUpdateActivityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.Bot.Connector;
+
+namespace Tomaszkiewicz.BotFramework.Extensions
+{
+    public static class ConversationUpdateActivityExtensions
+    {
+        public static bool AreMembersAdded(this IConversationUpdateActivity activity)
+        {
+            if (!activity.MembersAdded.Any())
+                return false;
+
+            var newMembers = activity.MembersAdded?.Where(t => t.Id != activity.Recipient.Id);
+
+            return newMembers != null && newMembers.Any();
+        }
+    }
+}
